175a4ac93d20f4ea86eddebfc81708e3a49a37c2
lilicorak <lilicorak@gmail.com>
2020-04-23 15:46:53 -0400

    Building the overview page

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-#install.packages("dplyr", "shiny", "shinyWidgets", "ggplot2", "scales", "shinythemes", "shinydashboardPlus")
+#install.packages("dplyr", "shiny", "shinyWidgets", "ggplot2", "scales", "shinythemes", "shinydashboardPlus", "DT", "ggsci")
 
 require(dplyr)
 require(shiny)
@@ -7,35 +7,28 @@ require(ggplot2)
 require(scales)
 require(shinythemes)
 require(shinydashboardPlus)
+require(DT)
+require(ggsci)
 
 # Read in the data
 read_data <- function (filename) {
   dat <- subset(read.csv(paste0("data/", filename, ".csv"), head=T, sep=","), 
-                select = c("VAR", "CLASS_FLAG", "PROV_RES", "SEX", "AGE", "WITH_VAL_0", "N", "MEAN", "NWGT", "SUM", 
+                select = c("year", "file", "PROV_RES", "VAR", "CLASS_FLAG", "SEX", "AGE", "WITH_VAL_0", "N", "MEAN", "NWGT", "SUM", 
                            "P75", "P25", "P_0", "P_10", "P_20", "P_30", "P_40", "P_50", "P_60", "P_70", "P_80", "P_90", "P_100"))
   
   dat$CLASS_FLAG <- factor(dat$CLASS_FLAG, levels = c("ALL", "1", "2", "3", "4", "5"))
   dat$PROV_RES <- factor(dat$PROV_RES, levels = c("ALL", "NL", "PE", "NS", "NB", "QC", "ON", "MB", "SK", "AB", "BC", "YT", "NT", "NU"))
   dat$SEX <- factor(dat$SEX, levels = c("ALL", "1", "2"))
   dat$AGE <- factor(dat$AGE, levels = c("All", "0-14", "15-19", "20-24", "25-34", "35-44", "45-54", "55-64", "65+", "Not Stated"))
-
+  
   return(dat)
 }
 
-APIM_2017 <- read_data("APIM_tab_2017")
-APIM_2018 <- read_data("APIM_tab_2018")
-
-CHS_2017 <- read_data("CHS_tab_2017")
-CHS_2018 <- read_data("CHS_tab_2018")
-
-CIS_2017 <- read_data("CIS_tab_2017")
-CIS_2018 <- read_data("CIS_tab_2018")
-
-CISPlus_2017 <- read_data("CISPlus_tab_2017")
-CISPlus_2018 <- read_data("CISPlus_tab_2018")
-
-SHS_2017 <- read_data("SHS_tab_2017")
-SHS_2018 <- read_data("SHS_tab_2018")
+APIM <<- rbind(read_data("APIM_tab_2017"), read_data("APIM_tab_2018"))
+CHS <<- rbind(read_data("CHS_tab_2017"), read_data("CHS_tab_2018"))
+CIS <<- rbind(read_data("CIS_tab_2017"), read_data("CIS_tab_2018"))
+CISPlus <<- rbind(read_data("CISPlus_tab_2017"), read_data("CISPlus_tab_2018"))
+SHS <<- rbind(read_data("SHS_tab_2017"), read_data("SHS_tab_2018"))
 
 
 # Set up the UI
@@ -69,36 +62,34 @@ ui <- (fluidPage(
                               selected = 2018),
                  selectInput("overviewVar",
                              "Variable:",
-                             choices = unique(APIM_2017$VAR),
+                             choices = unique(APIM$VAR),
                              selected = "TOTINC"),
                  selectInput("overviewClass",
                              "Class flag:",
-                             choices = unique(APIM_2017$CLASS_FLAG),
+                             choices = unique(APIM$CLASS_FLAG),
                              selected = "ALL"),
                  selectInput("overviewAge",
                              "Age group:",
-                             choices = unique(APIM_2017$AGE),
+                             choices = unique(APIM$AGE),
                              selected = "All"),
                  selectInput("overviewProv",
                              "Province of residence:",
-                             choices = unique(APIM_2017$PROV_RES),
-                             selected = "ALL")
+                             choices = unique(APIM$PROV_RES),
+                             selected = unique(APIM$PROV_RES),
+                             multiple = T)
                ),
                mainPanel(
                  width = 10,
-                 fluidRow(column(4, wellPanel(p("mean by prov"),
-                                              plotOutput("overviewMean"))),
-                          column(4, wellPanel(p("NGWT by prov"),
-                                              plotOutput("overviewNGWT"))),
-                          column(4, wellPanel(p("P50 by prov"),
-                                              plotOutput("overviewP50")))),
-                 fluidRow(boxPlus(title = "overview data table",
+                 fluidRow(column(4, plotOutput("overviewMean")),
+                          column(4, plotOutput("overviewNGWT")),
+                          column(4, plotOutput("overviewP50"))),
+                 fluidRow(boxPlus(title = "Data table",
                                   closable= F,
                                   collapsible = T,
                                   collapsed = T,
                                   status = "info",
                                   width = 12,
-                                  dataTableOutput("overviewData")))
+                                  DTOutput("overviewData")))
                )
              )),
   
@@ -118,19 +109,19 @@ ui <- (fluidPage(
                               selected = 2018),
                  selectInput("percentileVar",
                              "Variable:",
-                             choices = unique(APIM_2017$VAR),
+                             choices = unique(APIM$VAR),
                              selected = "TOTINC"),
                  selectInput("percentileClass",
                              "Class flag:",
-                             choices = unique(APIM_2017$CLASS_FLAG),
+                             choices = unique(APIM$CLASS_FLAG),
                              selected = "ALL"),
                  selectInput("percentileAge",
                              "Age group:",
-                             choices = unique(APIM_2017$AGE),
+                             choices = unique(APIM$AGE),
                              selected = "All"),
                  selectInput("percentileProv",
                              "Province of residence:",
-                             choices = unique(APIM_2017$PROV_RES),
+                             choices = unique(APIM$PROV_RES),
                              selected = "ALL")
                ),
                mainPanel(
@@ -162,7 +153,7 @@ ui <- (fluidPage(
                               selected = 2018),
                  selectInput("demoVar",
                              "Variable:",
-                             choices = unique(APIM_2017$VAR),
+                             choices = unique(APIM$VAR),
                              selected = "TOTINC"),
                  selectInput("demoStat",
                              "Statistic:",
@@ -171,15 +162,15 @@ ui <- (fluidPage(
                              selected = "MEAN"),
                  selectInput("demoClass",
                              "Class flag:",
-                             choices = unique(APIM_2017$CLASS_FLAG),
+                             choices = unique(APIM$CLASS_FLAG),
                              selected = "ALL"),
                  selectInput("demoAge",
                              "Age group:",
-                             choices = unique(APIM_2017$AGE),
+                             choices = unique(APIM$AGE),
                              selected = "All"),
                  selectInput("demoProv",
                              "Province of residence:",
-                             choices = unique(APIM_2017$PROV_RES),
+                             choices = unique(APIM$PROV_RES),
                              selected = "ALL")
                  ),
                mainPanel(
@@ -207,14 +198,44 @@ ui <- (fluidPage(
              )
     ))
 
+
+
 # Set up the server function
 server <- function(input, output, session) {
   
 ### Overview page
   
   # Create data file
+  overviewDataFile <- reactive({
+    overview <- do.call("rbind", lapply(input$overviewFiles, get))
+    overview <- subset(overview, year == input$overviewYear &
+                         VAR == input$overviewVar &
+                         CLASS_FLAG == input$overviewClass &
+                         AGE == input$overviewAge &
+                         SEX == "ALL" &
+                         PROV_RES %in% input$overviewProv)
+    return(overview)
+  })
   
+  output$overviewMean <- renderPlot({
+    ggplot(overviewDataFile()) + geom_col(aes(x=PROV_RES, y= MEAN, fill = file), position = "dodge") + theme_classic() + scale_fill_jama() +
+      scale_y_continuous(labels = comma) + theme(legend.position = "bottom") + labs(title="Mean by province and file")
+  })
   
+  output$overviewNGWT <- renderPlot({
+    ggplot(overviewDataFile()) + geom_col(aes(x=PROV_RES, y= NWGT, fill = file), position = "dodge") + theme_classic() + scale_fill_jama() +
+      scale_y_continuous(labels = comma) + theme(legend.position = "bottom") + labs(title="NWGT by province and file")
+  })
+
+  output$overviewP50 <- renderPlot({
+    ggplot(overviewDataFile()) + geom_col(aes(x=PROV_RES, y= P_50, fill = file), position = "dodge") + theme_classic() + scale_fill_jama() +
+      scale_y_continuous(labels = comma) + theme(legend.position = "bottom") + labs(title="P_50 by province and file")
+  })
+  
+  output$overviewData <- renderDT(
+    subset(overviewDataFile(), select = -c(SEX, AGE, WITH_VAL_0, CLASS_FLAG, year, VAR)), 
+                  options=list(paging = F, searching=F, scrollX = TRUE), rownames=F
+  )
   
 ### Percentiles page
   
